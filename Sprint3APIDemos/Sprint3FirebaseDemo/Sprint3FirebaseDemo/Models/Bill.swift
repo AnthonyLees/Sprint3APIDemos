53c29495b7c579f1803b995805c7d2c4dc8e2cd4
AnthonyLees <56968629+AnthonyLees@users.noreply.github.com>
2022-10-27 20:55:50 -0400

    Update Bill.swift

--- Sprint3APIDemos/Sprint3FirebaseDemo/Sprint3FirebaseDemo/Models/Bill.swift
+++ Sprint3APIDemos/Sprint3FirebaseDemo/Sprint3FirebaseDemo/Models/Bill.swift
@@ -12,6 +12,8 @@ struct Bill: Identifiable, Codable {
     
   // MARK: Fields
   @DocumentID var id: String?
+  var bill_owers: [String]
+  var bill_payers: [String]
   var date: String
   var description: String
   var title: String
@@ -19,6 +21,8 @@ struct Bill: Identifiable, Codable {
   // MARK: Codable
   enum CodingKeys: String, CodingKey {
     case id
+    case bill_owers
+    case bill_payers
     case date
     case description
     case title
